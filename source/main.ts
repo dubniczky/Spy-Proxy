2f4a8fc505e8856fa5a99e64d79b8736fd861256
Richard Nagy <nagy.richard.antal@gmail.com>
2022-01-11 15:39:40 +0100

    Updated config

--- source/main.ts
+++ source/main.ts
@@ -1,10 +1,12 @@
 'use strict'
 
+
 //Import external modules
 import express from 'express'
 import httpProxy from 'http-proxy'
 import config from 'config'
 
+//Import internal modules
 import proxyRequestEvent from './events/request'
 import proxyResponseEvent from './events/response'
 
@@ -21,21 +23,24 @@ proxy.on('proxyReq', proxyRequestEvent)
 proxy.on('proxyRes', proxyResponseEvent)
 console.log('Proxy loaded.')
 
+
 //Create express app
 const app = express();
 
-app.get('*', function(req, res)
+app.get('*', (req, res) =>
 {
     //@ts-ignore
     req.id = nextRequestID++
     proxy.web(req, res, { target: `${req.protocol}://${req.hostname}` })
 })
 
+
 //Globals
-let nextRequestID = 1
+let nextRequestID:number = 1
+let port:number = config.get<number>('ports.http')
 
-console.log('Server loaded.')
 
-const server = app.listen(config.get('ports.http'))
-console.log('Server running.')
+//Start
+const server = app.listen( port )
+console.log('Proxy listening on port ' + port)
 console.log('---------------')
